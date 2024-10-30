738281f1db23eee4ef96471f942eece5e2947580
DmitryKonovets0 <dmitrykonovets@gmail.com>
2024-10-30 16:12:31 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `PortfolioS`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components"]
+};
